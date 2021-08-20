011b7264a0cda4c968af1cf0c0852b9746a1f173
Dmitry Zaitsev <dmitry.zaitsev@tectus.by>
2021-08-20 18:27:20 +0300

    wip commit

--- cmd/root.go
+++ cmd/root.go
@@ -2,8 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/spf13/cobra"
 	"os"
+	"strings"
 )
 
 var rootCmd = &cobra.Command{
@@ -12,8 +15,18 @@ var rootCmd = &cobra.Command{
 	Long: `A Fast and Flexible Static Site Generator built with
                 love by spf13 and friends in Go.
                 Complete documentation is available at http://hugo.spf13.com`,
-	Run: func(cmd *cobra.Command, args []string) {
-		// Do Stuff Here
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		e := echo.New()
+		e.Use(middleware.Logger())
+		e.Use(middleware.Recover())
+
+		httpListen := ":8080"
+		if val, ok := os.LookupEnv("PORT"); ok && len(val) > 0 {
+			httpListen = strings.TrimSpace(val)
+		}
+
+		return e.Start(httpListen)
 	},
 }
 
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/machine-brest/k8s-helloworld
 
 go 1.16
 
-require github.com/spf13/cobra v1.2.1 // indirect
+require (
+	github.com/labstack/echo/v4 v4.5.0
+	github.com/spf13/cobra v1.2.1
+)
