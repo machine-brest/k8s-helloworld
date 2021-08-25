3d93e2b6e19e81a0b398d3e5a4588554e59ee929
Dmitry Zaitsev <dmitry.zaitsev@tectus.by>
2021-08-26 02:25:53 +0300

    added some tests

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/labstack/echo/v4 v4.5.0
 	github.com/spf13/cobra v1.2.1
+	github.com/stretchr/testify v1.7.0 // indirect
 )
---
+++ main_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestName(t *testing.T) {
+	require.NoError(t, nil)
+}
