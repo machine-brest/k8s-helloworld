23e0991e2261640d3cc53f849995c52400006d19
Dmitry Zaitsev <machine.brest@gmail.com>
2021-09-02 00:17:14 +0300

    updated infra

--- infra/main.tf
+++ infra/main.tf
@@ -11,5 +11,7 @@ terraform {
 }
 
 provider "aws" {
-  region = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,4 +1,3 @@
-/*
 variable "aws_access_key" {
   type        = string
   description = "AWS Access Key"
@@ -8,7 +7,6 @@ variable "aws_secret_key" {
   type        = string
   description = "AWS Secret Key"
 }
-*/
 
 variable "aws_region" {
   description = "AWS region"
