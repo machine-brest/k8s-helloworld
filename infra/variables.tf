0f00ada9e07b30bf02a99c34ab808a621008da98
Dmitry Zaitsev <machine.brest@gmail.com>
2021-09-01 23:00:29 +0300

    added infra

---
+++ infra/variables.tf
@@ -0,0 +1,13 @@
+variable "aws_access_key" {
+  type        = string
+  description = "AWS Access Key"
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS Secret Key"
+}
+
+variable "aws_region" {
+  description = "AWS region"
+}
